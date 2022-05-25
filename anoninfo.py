ab77f4e7129a9ed0fd898b6a0ae39ac867be8247
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-05-25 13:22:01 +0600

    Update anoninfo.py

--- anoninfo.py
+++ anoninfo.py
@@ -59,7 +59,8 @@ class anoninfoMod(loader.Module):
                 "custom_button1",
                 ["🔥 Apodiktum Hikka Modules 🔥", "https://t.me/apodiktum_modules"],
                 lambda: self.strings("_cfg_cst_btn"),
-                validator=loader.validators.Series(doc="ru", min_len=0, max_len=2),
+                validator=loader.validators.Series(min_len=0, max_len=2),
+                doc="ru"
             ),
             loader.ConfigValue(
                 "custom_button2",
